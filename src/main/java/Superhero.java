e04ff60f9a62168edcde82b56bf79171b7dd1ab8
mathiasfri <mathiasfridthjof@live.dk>
2022-09-12 11:21:06 +0200

    Oprettet klasse og tilføjet get/set metoder for heltenes info

---
+++ src/main/java/Superhero.java
@@ -0,0 +1,49 @@
+public class Superhero
+{
+    //Scanner sc = new Scanner(System.in);
+
+    private String heroName;
+    private String realName;
+    private String superpower;
+    private int age;
+
+    public Superhero(String heroName, String realName, String superpower, int age)
+    {
+        this.heroName = heroName;
+        this.realName = realName;
+        this.superpower = superpower;
+        this.age = age;
+    }
+
+    public String getHeroName(){
+        return heroName;
+    }
+
+    public void setHeroName(String heroName){
+        this.heroName = heroName;
+    }
+
+    public String getRealName(){
+        return heroName;
+    }
+
+    public void setRealName(String realName){
+        this.realName = realName;
+    }
+
+    public String getSuperpower(){
+        return superpower;
+    }
+
+    public void setSuperpower(String superpower){
+        this.superpower = superpower;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+}
