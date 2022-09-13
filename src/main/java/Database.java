7255bfca75fa022d7f9629b3dfab9836d71e1b49
mathiasfri <55091986+mathiasfri@users.noreply.github.com>
2022-09-13 15:54:47 +0200

    Tilføjet efterspørgsel på heltens informationer og sendt til Database efter informationer er oplyst. Også tilføjet et loop på koden, samt mulighed for afslutning.

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -1,24 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Database
 {
-    Scanner sc = new Scanner(System.in);
+    private ArrayList<Superhero> superheroes = new ArrayList();
 
-    private Superhero[] superheroes = new Superhero[5];
 
-    public Database()
+    public void addSuperhero(String heroName, String realName, String superpower, int age)
     {
-        System.out.println("Velkommen til din SuperHelte database!");
-        System.out.println("Menu - Vælg en valgmulighed");
-        System.out.println("1. Opret en Superhelt\n9. Afslut");
-
-        if (sc.nextInt() == 1)
-        {
-            // lav superhelt
-        }
-        if (sc.nextInt() == 9)
-        {
-            System.out.print("Du har nu afsluttet. Farvel");
-        }
+        Superhero superheroes = new Superhero(heroName, realName, superpower, age);
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,46 @@
+import java.util.Scanner;
+
 public class Main
 {
+    static Scanner sc = new Scanner(System.in);
     static Database db = new Database();
+    static int menuInput;
 
     public static void main(String[] args)
     {
-        System.out.print(db.toString());
+        do {
+            System.out.println("---------------------------------------------------");
+            System.out.println("Velkommen til din SuperHelte database!");
+            System.out.println("Menu - Vælg en valgmulighed");
+            System.out.println("""
+                    1. Opret en Superhelt
+                    2. Se liste over superhelte
+                    9. Afslut
+                    """);
+
+            int menuInput = sc.nextInt();
+
+            if (menuInput == 1) {
+                System.out.println("Hvad skal din superhelts heltenavn være?");
+                String heroName = sc.next();
+
+                System.out.println("Hvad er din superhelts rigtige navn?");
+                String realName = sc.next();
+                sc.nextLine();
+
+                System.out.println("Hvad er din superhelts superkraft?");
+                String superpower = sc.next();
+
+                System.out.println("Hvad er din superhelts alder?");
+                int age = sc.nextInt();
+
+                db.addSuperhero(heroName, realName, superpower, age);
+            }
+
+            if (menuInput == 9) {
+                System.out.print("Du har nu afsluttet. Farvel");
+                System.exit(0);
+            }
+        }while (menuInput != 9);
     }
 }
